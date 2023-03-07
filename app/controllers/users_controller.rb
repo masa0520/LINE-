3e16a9c5cce1cff6bd28709b0bb51bcda128ca5a
masa0520 <masahiro130520@icloud.com>
2023-03-07 19:19:30 +0900

    [add] ログイン機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
   before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /users/new
@@ -11,8 +12,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: "User was successfully created."
+      redirect_to root_path, notice: t('users.create.success')
     else
+      flash.now[:alert] = t('users.create.failure')
       render :new, status: :unprocessable_entity
     end
   end
@@ -20,7 +22,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     @user.destroy
-    redirect_to users_url, notice: "User was successfully destroyed."
+    redirect_to root, notice: "User was successfully destroyed."
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  root  "top#top"
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
