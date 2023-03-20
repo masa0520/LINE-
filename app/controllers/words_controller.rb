1cb30d3dce291a3745abf9baf177b14effe92577
masa0520 <masahiro130520@icloud.com>
2023-03-20 20:05:36 +0900

    [fix] 複数フォームの日英単語の登録

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -10,53 +10,50 @@ class WordsController < ApplicationController
   end
   
   def create
-    #英語のパラメーターを取得
-    input_english = params[:english_word][:english]
-    #スペースで区切った英語を配列に格納
-    array_english = input_english.split(/[[:space:]]/)
-
-    #日本語のパラメーターを取得
-    input_japanese = params[:japanese_word][:japanese]
-    #スペースで区切った日本語を配列に格納
-    array_japanese = input_japanese.split(/[[:space:]]/)
-    #英語が一つで日本語が2つ以上だったら
-    if array_english.length == 1 && array_japanese.length >= 2
-      #英語を作成
-      @english_word = current_user.english_words.create(english_word_params)
-      #eaachで日本語を一つずつ取り出し作成,英語と結びつけて中間テーブルに保存
-      array_japanese.each do |word|
-        @japanese_word = current_user.japanese_words.create(japanese: word)
-        @english_word.japanese_words << @japanese_word
-      end
-      redirect_to words_path
-    #日本語が１つで英語が2つ以上だったら
-    elsif array_english.length >= 2 && array_japanese.length == 1
-      #日本語を作成
-      @japanese_word = current_user.japanese_words.create(japanese_word_params)
-      #eaachで英語を一つずつ取り出し作成,日本語と結びつけて中間テーブルに保存
-      array_english.each do |word|
-        @english_word = current_user.english_words.create(english: word)
-        @japanese_word.english_words << @english_word
-      end
-      redirect_to words_path
-    #英語が1つで日本語も１つだったら
-    elsif array_english.length && array_japanese.length == 1
-      @english_word = current_user.english_words.create(english_word_params)
-      @japanese_word = current_user.japanese_words.create(japanese_word_params)
-      @english_word.japanese_words << @japanese_word
-      redirect_to words_path
-    else
-      render :new
+    array_english = []
+    array_japanese = []
+    10.times do |i|
+      #英語のパラメーターを取得
+      input_english = params[:english][i.to_s]
+      #スペースで区切った英語を配列に格納
+      array_english << input_english.split(/[[:space:]]/)
+      #日本語のパラメーターを取得
+      input_japanese = params[:japanese][i.to_s]
+      #スペースで区切った日本語を配列に格納
+      array_japanese << input_japanese.split(/[[:space:]]/)
     end
-  end
-  
-  private
-  
-  def english_word_params
-    params.require(:english_word).permit(:english)
-  end
 
-  def japanese_word_params
-    params.require(:japanese_word).permit(:japanese)
+    array_english.zip(array_japanese).each do |english, japanese|
+      #両方nilだったら
+      next if english.empty? && japanese.empty?
+      #両方2つ以上の意味を持っていたら
+      next if english.length >= 2 && japanese.length >= 2
+      #英語がnilで日本語があったら
+      next if english.empty? && japanese.present?
+      #日本語がnilで英語があったら
+      next if japanese.empty? && english.present?
+      
+      #英語が一つで日本語が2つ以上だったら
+      if english.length == 1 && japanese.length >= 2
+        english_word = current_user.english_words.create(english: english[0])
+        japanese.each do |word|
+          japanese_word = current_user.japanese_words.create(japanese: word)
+          english_word.japanese_words << japanese_word
+        end
+      #日本語が１つで英語が2つ以上だったら
+      elsif japanese.length == 1 && english.length >= 2
+        japanese_word = current_user.japanese_words.create(japanese: japanese[0])
+        english.each do |word|
+          english_word = current_user.english_words.create(english: word)
+          japanese_word.english_words << english_word
+        end
+      #英語が1つで日本語も１つだったら
+      else english.length == 1 && japanese.length == 1
+        english_word = current_user.english_words.create(english: english[0])
+        japanese_word = current_user.japanese_words.create(japanese: japanese[0])
+        english_word.japanese_words << japanese_word
+      end
+    end
+    redirect_to words_path
   end
 end
