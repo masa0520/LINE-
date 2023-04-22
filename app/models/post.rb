cf1b28bc789091f5eaa8d9a4ad48adf7f219b876
masa0520 <masahiro130520@icloud.com>
2023-04-23 01:29:03 +0900

    [fix] 不要なコードだったので削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,10 +8,6 @@ class Post < ApplicationRecord
   
   validates :title, presence: true
 
-  def bookmarked_by?(user)
-    bookmarks.where(user_id: user).exists?
-  end
-
   def self.ransackable_attributes(auth_object = nil)
     ["genre_id"]
   end
